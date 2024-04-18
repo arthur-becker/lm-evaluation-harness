c151b7bc0121b8161870cde40b2014e2a9700db5
Arthur Becker <arthur.becker@rwth-aachen.de>
2024-04-18 16:28:09 +0200

    fix the response length

--- lm_eval/models/gguf.py
+++ lm_eval/models/gguf.py
@@ -53,6 +53,7 @@ class GGUFLM(LM):
                     "prompt": prompt,
                     "logprobs": self.logprobs,
                     "temperature": self.temperature,
+                    "max_tokens": self.max_length,
                 }
                 if continuation:
                     prompt += continuation
